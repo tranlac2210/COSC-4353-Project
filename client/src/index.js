4cd920c4dc0a681f16525342c256f817551e4361
Phat Tran <66543973+raymoon-code@users.noreply.github.com>
2023-02-07 12:09:38 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WelcomeGuest from './guests/pages/WelcomeGuest';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -8,6 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App/>
     <WelcomeGuest/>
   </React.StrictMode>
 );
@@ -15,4 +18,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
