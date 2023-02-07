d626d4892796e83f73df2c8d1e72fa944bc2087f
Phat Tran <dtran48@CougarNet.UH.EDU>
2023-02-07 11:41:40 -0600

    Update Pages

---
+++ client/src/ClientPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link, useHistory} from 'react-router-dom';
+
+
+
+
+function ClientPage() {
+  return (
+    <div>
+      <h2>Welcome, Client!</h2>
+      <p>Here you can access your account information, view your recent transactions, and manage your personal information.</p>
+      <ul>
+        <li><a href="#">View Account Information</a></li>
+        <li><a href="#">View Recent Transactions</a></li>
+        <li><Link to="/ClientProfilePage">Manage Personal Information</Link></li>
+        
+      </ul>
+    </div>
+  );
+}
+
+export default ClientPage;
