8d7aa4af85c0ef1ce9e34c805b037c83faf629c4
tranlac2210 <tranlac3107@gmail.com>
2023-02-22 18:10:44 -0600

    Modified css files

--- client/src/pages/ChangePassPage.js
+++ client/src/pages/ChangePassPage.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
-import styled from "styled-components";
-import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-
+import "../styles/ChangePassPage.css"
 
 function ChangePassPage() 
- {
+{
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [showSuccessLabel, setShowSuccessLabel] = useState(false);
@@ -15,7 +11,7 @@ function ChangePassPage()
   const handleSubmit = (event) => {
     event.preventDefault();
 
-       if (password !== confirmPassword) {
+      if (password !== confirmPassword) {
       setShowSuccessLabel(false);
       
       setSuccessLabel("New password and confirm password not match!!!");
@@ -34,14 +30,11 @@ function ChangePassPage()
         
         return;
       }
-    
-
-   
   };
 
   return (
-    <Wrapper>
-        <h2></h2>
+    <>
+    <h2></h2>
     <form className='change_pass_form' onSubmit={handleSubmit}>
       <h2>Change Password</h2>
       <div>
@@ -67,111 +60,13 @@ function ChangePassPage()
       {showSuccessLabel && (
         <label className="success-label">{showSuccessLabel}</label>
       )}
-       {SuccessLabel && (
+      {SuccessLabel && (
         <label className="error-label">{SuccessLabel}</label>
       )}
       <button className='change_pass_button' type="submit">Change Password</button>
     </form>
-   
-    </Wrapper>
+    </>
   );
 };
-
-const Wrapper = styled.div`
-
-
-.select_box{
-    margin-bottom: 20px;
-  }
-
-  .success-label{
-    color:green;
-  }
-
-  .error-label{
-    color:red;
-  }
-
-  .profileh1{
-    text-align: center;
-    margin-bottom: 15px;
-  }
-  .change_pass_form {
-    
-    background: linear-gradient(to right, #fa709a 0%, #fee140 100%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #f2f2f2;
-    padding: 70px;
-    border-radius: 20px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    width: 40%;
-    margin: auto;
-  }
-  
-  h2 {
-    text-align: center;
-    margin-top: 10px;
-    margin-bottom: 30px;
-  } 
-
-
-  .login_form h2 {
-    font-size: 40px;
-    margin-bottom: 30px;
-  }
-
-  .change_pass_label,.login_pass {
-    font-size: 24px;
-    margin-bottom: 10px;
-    margin-right: 20px;
-  }
-  
-  .change_pass_input {
-    
-    width: 300px;
-    height: 40px;
-    padding: 10px;
-    margin-bottom: 20px;
-    border-radius: 25px;
-    border: none;
-    font-size: 16px;
-  }
-  
-  .change_pass_button {
-    margin-top: 20px;
-    width: 300px;
-    height: 40px;
-    padding: 10px;
-    border-radius: 25px;
-    border: none;
-    font-size: 16px;
-    background-color: #4CAF50;
-    color: white;
-    cursor: pointer;
-  }
-
-  .demo{
-    margin-top: 20px;
-
-    padding: 10px;
-    border-radius: 5px;
-    border: none;
-    font-size: 16px;
-    background-color: #4CAF50;
-    color: white;
-    cursor: pointer;
-  }
-  .demo:hover {
-    background-color: #3e8e41;
-  }
-  
-  .change_pass_button:hover {
-    background-color: #3e8e41;
-  }
-
-
-`;
 
 export default ChangePassPage;
--- client/src/pages/FuelQuoteForm.js
+++ client/src/pages/FuelQuoteForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from "styled-components";
+import "../styles/FuelQuoteForm.css";
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 
@@ -18,7 +18,7 @@ function FuelQuoteForm() {
 
 
   return (
-    <Wrapper>
+    <>
     <div><h1 className='profiletest'></h1>
       <form className='fuelinput' onSubmit={handleSubmit}>
       <h2>Fuel Quote Form</h2>
@@ -66,57 +66,12 @@ function FuelQuoteForm() {
         <button className="submit_button" type="submit">Request Quote</button>
       </form>
     </div>
-    </Wrapper>
+    </>
       
 
 
     
   );
 }
-
-const Wrapper = styled.div`
-  .fuelinput {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #f2f2f2;
-    padding: 30px;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    width: 50%;
-    margin: auto;
-  }
-  
-  .fuelinput h2 {
-    margin-bottom: 30px;
-  }
-  
-  .input {
-    display: flex;
-    margin-top: 10px;
-    margin-bottom: 5px;
-    align-items: center;
-    
-  }
-
-  .input1 {
-    width: 100%;
-    padding: 5px;
-    border-radius: 5px;
-    border: none;
-    margin-bottom: 20px;
-  }
-
-  .submit_button {
-    margin-top: 20px;
-    background-color: #4CAF50;
-    color: white;
-    padding: 5px 20px;
-    border-radius: 5px;
-    border: none;
-    cursor: pointer;
-  }
-
-`;
 
 export default FuelQuoteForm;
