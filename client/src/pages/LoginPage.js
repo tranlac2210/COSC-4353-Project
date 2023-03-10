9de679b17a9de07f372f90b1d8426ce16bfa62c8
Chuong Tran <chuongtran@Chuongs-MacBook-Pro.local>
2023-03-10 16:41:42 -0600

    login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ import { Submenu } from '../components'
 import { AppProvider } from '../components';
 
 
-function LoginPage(){
+function LoginPage({onBack, onClose}){
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -26,57 +26,58 @@ function LoginPage(){
 
   return (
     <>
-      
-      <div className="wholeloginpage" onclick="window.location.href='/';"> 
-      
+      {/* onclick="window.location.href='/';" */}
+      <div className="wholeloginpage overlay" onClick={onClose}> 
+        <div onClick={(e) => {e.stopPropagation()}}>
+        <form className="c_login_form" onSubmit={handleSubmit}>
+          <button onClick={onBack} className="loginbb" href="/" id="bottle" onclick="document.location=this.id+'.html';return false;">
+            <img src="https://cdn.discordapp.com/attachments/722016314679361559/1078414402761527417/image_3.png" alt="bottle"/>
+          </button>
+
+          <div className="login-background">
+            <img src="https://cdn.discordapp.com/attachments/722016314679361559/1075986201259036722/login.jpg" alt="Login Image"/>
+          </div>
+  
+        
+          <h3>Log in</h3>
+          <div >
+            <label className="login_username" htmlFor="username">Username:</label>
+            <input className="login_input"
+              type="text"
+              id="username"
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+            />
+          </div>
+          <div>
+            <label className="login_pass" htmlFor="password">Password:</label>
+            <input className="login_input"
+              type="password"
+              id="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+            />
+          </div>
+          {ErrorLabel && (<label className="error-label"> {ErrorLabel}</label>)}
+
+          <button className="login_button" type="submit"><h4>Submit</h4></button>
           
-      <form className="c_login_form" onSubmit={handleSubmit}>
-        <a className="loginbb" href="/" id="bottle" onclick="document.location=this.id+'.html';return false;">
-        <img src="https://cdn.discordapp.com/attachments/722016314679361559/1078414402761527417/image_3.png" alt="bottle"/>
-        </a>
-      <div className="login-background">
-        
-        <img src="https://cdn.discordapp.com/attachments/722016314679361559/1075986201259036722/login.jpg" alt="Login Image"/>
-      </div>
- 
-      
-        <h3>Log in</h3>
-        <div >
-          <label className="login_username" htmlFor="username">Username:</label>
-          <input className="login_input"
-            type="text"
-            id="username"
-            value={username}
-            onChange={event => setUsername(event.target.value)}
-          />
+          
+          
+          
+          <div className="already-have-account">
+          Not a member yet? 
+          <a className="signup_now" href="/SignupPage">Create New Account</a>
         </div>
-        <div>
-          <label className="login_pass" htmlFor="password">Password:</label>
-          <input className="login_input"
-            type="password"
-            id="password"
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
-        </div>
-        {ErrorLabel && (<label className="error-label"> {ErrorLabel}</label>)}
-
-        <button className="login_button" type="submit"><h4>Submit</h4></button>
-        
-        
-        
-        
         <div className="already-have-account">
-        Not a member yet? 
-        <a className="signup_now" href="/SignupPage">Create New Account</a>
-      </div>
-      <div className="already-have-account">
-        Admin log in - 
-        <a className="signup_now" href="/AdminLoginPage">Click here</a>
-      </div>
-        <Link className="demo" to="/ClientPage">Continue as Guest</Link>
+          Admin log in - 
+          <a className="signup_now" href="/AdminLoginPage">Click here</a>
+        </div>
+          <Link className="demo" to="/ClientPage">Continue as Guest</Link>
 
-      </form>
+        </form>
+        </div>
+        
       </div>
     </>
     
