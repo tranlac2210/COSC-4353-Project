12a69b686ff34233b76fdf47d83d0a9deb80af32
Chuong Tran <chuongtran@Chuongs-MacBook-Pro.local>
2023-03-09 19:02:24 -0600

    hash changepassword

--- server/src/routes/user/user.route.js
+++ server/src/routes/user/user.route.js
@@ -1,15 +1,15 @@
 import express from "express";
-import {signUp, signIn} from "./user.controller.js"
+import {signUp, signIn, getUsers, passwordChange} from "./user.controller.js"
 
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json("User Hello")
-})
+router.get('/getUsers', getUsers);
 
 router.post('/signup', signUp);
 
 router.post('/signin', signIn)
+
+router.put('/passwordChange', passwordChange)
 
 export default router;
