ee8682e8eabdb016f85f18b0406aa1fad94b45db
Chuong Tran <chuongtran@Chuongs-MacBook-Pro.local>
2023-03-16 00:50:16 -0500

    new user data

---
+++ server/src/routes/data/users.js
@@ -0,0 +1,88 @@
+const users = [
+  {
+    id: "1",
+    userName: "chuong",
+    password: "1234",
+    active: 1,
+    info: {
+      FullName: "chuong tran",
+      Address1: "add",
+      Address2: "",
+      city: "Houston",
+      State: "Texas",
+      Zipcode: "77204",
+    },
+    orders: [
+        {
+          id: 1,
+          name: "95",
+          total: 200,
+        },
+      ],
+  },
+  {
+    id: "2",
+    userName: "chuong2",
+    password: "1234",
+    active: 1,
+    info: {
+      FullName: "si thue",
+      Address1: "add2",
+      Address2: "",
+      city: "Houston2",
+      State: "Texas2",
+      Zipcode: "772042",
+    },
+    orders: [
+      {
+        id: 1,
+        name: "95",
+        total: 200,
+      },
+    ],
+  },
+  {
+    id: "3",
+    userName: "chuong3",
+    password: "1234",
+    active: 1,
+    info: {
+      FullName: "rau con",
+      Address1: "add2",
+      Address2: "",
+      city: "Houston2",
+      State: "Texas2",
+      Zipcode: "772042",
+    },
+    orders: [
+      {
+        id: 1,
+        name: "95",
+        total: 200,
+      },
+    ],
+  },
+  {
+    id: "4",
+    userName: "chuong4",
+    password: "1234",
+    active: 1,
+    info: {
+      FullName: "ronaldo",
+      Address1: "add2",
+      Address2: "",
+      city: "Houston2",
+      State: "Texas2",
+      Zipcode: "772042",
+    },
+    orders: [
+      {
+        id: 1,
+        name: "95",
+        total: 200,
+      },
+    ],
+  },
+];
+
+export default users;
