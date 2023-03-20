7a0fc48a05661e660554c88923957ebd500ce376
Chuong Tran <chuongtran@Chuongs-MacBook-Pro.local>
2023-03-20 17:57:50 -0500

    get orders

--- server/src/routes/data/users.js
+++ server/src/routes/data/users.js
@@ -3,7 +3,7 @@ const users = [
     id: "1",
     userName: "chuong",
     password: "1234",
-    active: 0,
+    active: 1,
     info: {
       FullName: "chuong tran",
       Address1: "add",
@@ -15,8 +15,27 @@ const users = [
     orders: [
         {
           id: 1,
-          name: "95",
-          total: 200,
+          name: "Regular",
+          volumn: 20,
+          date: "2019-02-03T06:48:07"
+        },
+        {
+          id: 2,
+          name: "Mid-grade",
+          volumn: 36,
+          date: "2020-02-04T06:48:07"
+        },
+        {
+          id: 3,
+          name: "Premium",
+          volumn: 15,
+          date: "2020-02-10T06:48:07"
+        },
+        {
+          id: 1,
+          name: "Regular",
+          volumn: 23,
+          date: "2020-02-17T06:48:07"
         },
       ],
   },
@@ -36,8 +55,27 @@ const users = [
     orders: [
       {
         id: 1,
-        name: "95",
-        total: 200,
+        name: "Regular",
+        volumn: 20,
+        date: "2023-02-03T06:48:07"
+      },
+      {
+        id: 2,
+        name: "Mid-grade",
+        volumn: 40,
+        date: "2023-02-10T06:48:07"
+      },
+      {
+        id: 3,
+        name: "Premium",
+        volumn: 25,
+        date: "2023-03-03T06:48:07"
+      },
+      {
+        id: 1,
+        name: "Regular",
+        volumn: 13,
+        date: "2023-03-10T06:48:07"
       },
     ],
   },
@@ -57,8 +95,27 @@ const users = [
     orders: [
       {
         id: 1,
-        name: "95",
-        total: 200,
+        name: "Regular",
+        volumn: 22,
+        date: "2018-02-03T06:48:07"
+      },
+      {
+        id: 2,
+        name: "Mid-grade",
+        volumn: 46,
+        date: "2019-02-03T12:48:07"
+      },
+      {
+        id: 3,
+        name: "Premium",
+        volumn: 25,
+        date: "2019-02-10T13:48:07"
+      },
+      {
+        id: 1,
+        name: "Regular",
+        volumn: 33,
+        date: "2019-03-03T14:48:07"
       },
     ],
   },
@@ -78,8 +135,27 @@ const users = [
     orders: [
       {
         id: 1,
-        name: "95",
-        total: 200,
+        name: "Regular",
+        volumn: 20,
+        date: "2021-01-03T06:48:07"
+      },
+      {
+        id: 2,
+        name: "Mid-grade",
+        volumn: 36,
+        date: "2021-02-03T010:48:07"
+      },
+      {
+        id: 3,
+        name: "Premium",
+        volumn: 15,
+        date: "2021-03-03T08:48:07"
+      },
+      {
+        id: 1,
+        name: "Regular",
+        volumn: 23,
+        date: "2021-02-010T07:48:07"
       },
     ],
   },
